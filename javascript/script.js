a42b1ffb9833f3ebbb7f6352c457ce0bf4293fd5
Bruno Ribeiro <bruno.ribeiro@credsystem.com.br>
2025-05-15 17:21:07 -0300

    adicionando cenário de fundo, função e animação de comer e dormir

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,49 @@
 canva = new Canvas(200,400,document.getElementById('canva'));
 tamagotchi = new Tamagotchi(50,50,false,true,canva.conteudo.getContext('2d'));
 var px=120;
-var py=80;
+var py=120;
 let andarX = 10;
 let andarY = 0;
 let altCanvas=200;
 let lagCanvas=400;
+//sprite pet
 const alturaSprite = 37.4;
 const larguraSprite = 37.5;
 let linhaSprite = 1;
+const CORRENDO_DIREITA = 1;
+const CORRENDO_ESQUERDA = 3;
+const DORMINDO = 7;
 let colunaSprite = 0;
+//sprite food
+const tamSpriteFood = 75;
+let linhaSpriteFood = 0;
+let colunaSpriteFood = 0;
+//acoes
 let stop = false;
+let eating = false;
+//image
 const image = new Image(50, 35); // Using optional size for image
 // Load an image of intrinsic size 300x227 in CSS pixels
 image.src = "./assets/dog-sprite.png";
 
+//imagem cenário
+const cenarioImage = new Image();
+cenarioImage.src = "./assets/floresta.jpg";
+
+//imagem comida
+const foodImage = new Image();
+foodImage.src = "./assets/foods.png";
+
 tamagotchi.exist.fillStyle = 'red';
 function criar(){
-    if(linhaSprite == 7) {
+    //DELIMITA COLUNAS DA LINHA DORMINDO
+    if(linhaSprite == DORMINDO) {
         if(colunaSprite > 1) {
             colunaSprite = 0;
         }
     }
-    if(linhaSprite == 1 || linhaSprite == 3) {
+    //DELIMITA COLUNAS DAS LINHAS CORRENDO
+    if(linhaSprite == CORRENDO_DIREITA || linhaSprite == CORRENDO_ESQUERDA) {
         if(colunaSprite > 3) {
             colunaSprite = 0;
         }
@@ -32,7 +53,24 @@ function criar(){
     //px eixo y de onde se inicia o sprite
     let posicaoInicialY = linhaSprite * alturaSprite;
     tamagotchi.exist.clearRect(0,0,canva.lag,canva.alt);
+    tamagotchi.exist.drawImage(cenarioImage, 0, 0, canva.lag, canva.alt);
     tamagotchi.exist.drawImage(image, posicaoInicialX, posicaoInicialY, larguraSprite, alturaSprite, px, py, tamagotchi.alt, tamagotchi.lag)
+    
+    if(eating) {
+        let posicaoInicialX = colunaSpriteFood * tamSpriteFood;
+        let posicaoInicialY = linhaSpriteFood * tamSpriteFood;
+        tamagotchi.exist.drawImage(foodImage, posicaoInicialX, posicaoInicialY, tamSpriteFood, tamSpriteFood, px, py, tamagotchi.alt, tamagotchi.lag)
+        colunaSpriteFood++;
+        if (colunaSpriteFood > 2) {
+            colunaSpriteFood = 0;
+            linhaSpriteFood++;
+        }
+        if (linhaSpriteFood > 2) {
+            colunaSpriteFood = 0;
+            linhaSpriteFood = 0;
+            eating = false;
+        }
+    }
     // tamagotchi.exist.fillRect(px, py, tamagotchi.alt, tamagotchi.lag);
 
     colunaSprite++;
@@ -42,12 +80,12 @@ function criar(){
         px+=andarX;
     }
 
-    if(px >= lagCanvas-tamagotchi.lag*2 || px == tamagotchi.lag){
+    if(px >= lagCanvas-(larguraSprite * 2) || px <= tamagotchi.lag){
         andarX *= -1;
-        if (linhaSprite == 1)
-            linhaSprite = 3;
-        else if (linhaSprite == 3)
-            linhaSprite = 1;
+        if (linhaSprite == CORRENDO_DIREITA)
+            linhaSprite = CORRENDO_ESQUERDA;
+        else if (linhaSprite == CORRENDO_ESQUERDA)
+            linhaSprite = CORRENDO_DIREITA;
     }
 }
 
@@ -61,9 +99,7 @@ function emagrece() {
 }
 
 function eat() {
-    tamagotchi.alt += 10;
-    tamagotchi.lag += 10;
-    setTimeout(emagrece, 200);
+    eating = true;
 }
 
 function sleep(){
